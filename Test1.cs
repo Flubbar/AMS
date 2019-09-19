29e8a5d28e0f84b7e2ed2c90caf2b49e5d3e64e5
이승연 <50094894+leeseungyeon002@users.noreply.github.com>
2019-09-19 14:08:12 +0900

    20190919 CLASS 실습1
    사용자로부터 입력한 수식을 계산하는 프로그램을 작성하여 보자

--- Test1.cs
+++ Test1.cs
@@ -1,4 +1,9 @@
-﻿//2018038002이승연
+﻿/* **********************************************
+* 프로그래명 : Test1.cs
+• 작성자 : 2018038002 이승연
+• 작성일 : 2019.09.19
+*프로그램 설명 : 사용자로부터 입력한 수식을 계산하는 프로그램을 작성하여 보자
+************************************************/
 using System;
 namespace App
 {
@@ -6,6 +11,8 @@ namespace App
     {
         static void Main(string[] args)
         {
+            //피연산자1(값1) 연산자 피연산자2(값2) 로 구 성되며, 연산자는 *,+,‐,/를 지원함
+            //정수형 형변환 int.Parse(Console.ReadLine());
             Console.Write("첫번째 숫자를 입력하세요 : ");
             int num1 = int.Parse(Console.ReadLine());
             Console.Write("연산자를 입력하세요 (+,-,*,/) : ");
@@ -13,7 +20,8 @@ namespace App
             Console.Write("두번째 숫자를 입력하세요 : ");
             int num2 = int.Parse(Console.ReadLine());
 
-            switch(op)
+            //switch/case
+            switch (op)
             {
                 case '+':
                     Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
