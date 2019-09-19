fe44f7bff667d19a275921a99c3f34c0311e43d0
이승연 <50094894+leeseungyeon002@users.noreply.github.com>
2019-09-19 14:01:44 +0900

    Add files via upload

---
+++ Test1.cs
@@ -0,0 +1,33 @@
+﻿//2018038002이승연
+using System;
+namespace App
+{
+    class Test1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("첫번째 숫자를 입력하세요 : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("연산자를 입력하세요 (+,-,*,/) : ");
+            char op = char.Parse(Console.ReadLine());
+            Console.Write("두번째 숫자를 입력하세요 : ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            switch(op)
+            {
+                case '+':
+                    Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+                    break;
+                case '-':
+                    Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+                    break;
+                case '*':
+                    Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+                    break;
+                case '/':
+                    Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+                    break;
+            }
+        }
+    }
+}
---
+++ Test2.cs
@@ -0,0 +1,36 @@
+﻿//2018038002이승연
+using System;
+namespace App
+{
+    class Student
+    {
+        public double kor;
+        public double math;
+        public double eng;
+
+        public void setScore()
+        {
+            Console.Write("국어성적 : ");
+            kor = double.Parse(Console.ReadLine());
+            Console.Write("수학성적 : ");
+            math = double.Parse(Console.ReadLine());
+            Console.Write("영어성적 : ");
+            eng = double.Parse(Console.ReadLine());
+        }
+        public double getAverage()
+        {
+            return (kor + math + eng) / 3.0;
+        }
+    }
+
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Student kim = new Student();
+            kim.setScore();
+            Console.WriteLine("과목의 평균은 : = {0}", kim.getAverage());
+        }
+    }
+}
