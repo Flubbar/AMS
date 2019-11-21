94cc030df45ae8078ef53d4daee73274aa0173ca
leeseungyeon002 <prizentivselfditerminizm@gmail.com>
2019-11-21 13:20:06 +0900

    .
    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -29,3 +30,36 @@ public class GameManager : MonoBehaviour{
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
+=======
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour{
+
+    bool gameHadEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHadEnded == false)
+        {
+            gameHadEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+>>>>>>> Stashed changes
